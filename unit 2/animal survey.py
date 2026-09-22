9f4879bd0d7a9cd737b45ac70d1e9facfaea64d5
ChaseDallin <821346@mystma.org>
2026-09-22 14:10:54 -0500

    animal survey

---
+++ unit 2/animal survey.py
@@ -0,0 +1,11 @@
+animal = input( "what is your favorite amimal\n> ")   
+like = input( "what do you like about them\n> ")
+live = input( "where do they live\n> ")
+fact = input( "what is a fun fact about this animal\n> ")
+
+
+print("--------------------------------------------------------------------")
+
+
+print( "your favorite animal is " +  animal + ". " + " what you like about your animal is " + like + ". " + "your animal lives in " + live + ". " + "your fun fact about your animal is "+ fact + ". ")
+      
---
+++ unit 2/space shuttle.py
@@ -0,0 +1,25 @@
+countdown = input("how many seconds are left on the timer\n") 
+
+oxygen_tanks = input("how many oxygen tanks are left\n")
+
+food_packs = input( "how many food packs do you have\n")
+
+water_packs = input( "how many water packs are stored on the ship\n")
+
+print ("countdown: " + countdown)
+
+print ("oxygen tanks: " + oxygen_tanks)
+
+print ("food packs: " + food_packs)
+
+print ("water packs: " + water_packs)
+
+oxygen_tanks = input("conferm oxygen tanks\n")
+
+print ("countdown: " + countdown)
+
+print ("oxygen tanks: " + oxygen_tanks)
+
+print ("food packs: " + food_packs)
+
+print ("water packs: " + water_packs)
