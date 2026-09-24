f9d61232b913a04d3e97875021b003788c0f9fdc
ChaseDallin <821346@mystma.org>
2026-09-24 13:45:32 -0500

    madlib-2 grade this one

--- unit 2/mad lib.py
+++ unit 2/mad lib.py
@@ -9,4 +9,4 @@ noun_1 = input (" Name a noun\n>")
 noun_2 = input (" Name another noun\n>")
 verb_1 = input ("Name a verb\n")
 
-print( "Ones upon a time, there was a village. In this " + adjective_1 + " village, there lived a " + adjective_2 + " " + noun_1 + ". It loved to " + noun_2 + " " + verb_1 + ". THE END!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ")
+print( "Ones upon a time, there was a village. In this " + adjective_1 + " village, there lived a " + adjective_2 + " " + noun_1 + ". It loved to " + noun_2 + " "+ verb_1 + ". THE END!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ")
