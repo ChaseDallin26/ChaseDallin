8fe48e3474b00d72f05984240d9a32a99bfd3274
ChaseDallin <821346@mystma.org>
2026-09-24 13:44:14 -0500

    animal survy

---
+++ unit 2/mad lib.py
@@ -0,0 +1,12 @@
+# Ones upon a time, there was a village.  
+# In this [adjective] village, there lived a [adjective] [noun].
+# It loved to [noun] and [verb].
+# The end
+
+adjective_1 = input (" Name a adjective\n>")
+adjective_2 = input (" Name another adjective\n>")
+noun_1 = input (" Name a noun\n>")
+noun_2 = input (" Name another noun\n>")
+verb_1 = input ("Name a verb\n")
+
+print( "Ones upon a time, there was a village. In this " + adjective_1 + " village, there lived a " + adjective_2 + " " + noun_1 + ". It loved to " + noun_2 + " " + verb_1 + ". THE END!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ")
